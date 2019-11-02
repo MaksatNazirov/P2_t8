b96690a125dbe6e66049a9c6d3d3ad42c79e8d02
Maks <nazirov.maksat96@gmail.com>
2019-11-02 09:02:10 +0600

    P2_t8

---
+++ Task8.py
@@ -0,0 +1,8 @@
+# #Given two integers A and B (A ≤ B). Print all numbers from A to B inclusively.
+# a=int(input())
+# b=int(input())
+# string=""
+# for a in range(a,b+1):
+#     print(a,end="232 ")
+
+
